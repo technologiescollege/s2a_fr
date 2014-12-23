2ce929b19abbbd9c47b675fe139bbc3ba7ad762b
SebCanet <sebastien.canet@ac-nantes.fr>
2014-12-23 15:47:55 +0100

    v2 !

--- Scratch2.bat
+++ Scratch2.bat
@@ -4,4 +4,4 @@ rem fichiers BAT et fork créés par Sébastien CANET
 cls
 SET currentpath=%~dp1
 SET dossier_scratch="Scratch 2.exe"
-start %dossier_scratch% %currentpath%bibliotheque\fichier_vide.sb2
+start %dossier_scratch% %currentpath%bibliotheque\fichier_depart_s2a.sb2
