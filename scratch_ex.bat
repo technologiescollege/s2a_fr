0af36dc59ccc2dae9cbac92571d8709a1e6c98de
SebCanet <sebastien.canet@ac-nantes.fr>
2014-12-26 02:34:59 +0100

    simplification mineure

--- scratch_ex.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-break ON
-rem fichiers BAT et fork créés par Sébastien CANET
-cls
-SET currentpath=%~dp1
-SET dossier_scratch="Scratch 2.exe"
-start %dossier_scratch% "F:\ZZZzzz\Projets Borland Studio\s2a\Debug_Build\bibliotheque\fichier_depart_s2a.sb2"
